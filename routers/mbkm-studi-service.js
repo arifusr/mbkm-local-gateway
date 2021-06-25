b99e53f5f8e24dc28f86720be48ffba63eb47030
arifusr <arifusr@gmail.com>
2021-06-25 08:10:45 +0700

    update service

--- routers/mbkm-studi-service.js
+++ routers/mbkm-studi-service.js
@@ -3,29 +3,25 @@ var express = require('express');
 var router = express.Router()
 const apiAdapter = require('./apiAdapter')
 
-const BASE_URL = 'http://localhost:8085'
+const BASE_URL = 'http://localhost:8083'
 const api = apiAdapter(BASE_URL)
 
-router.all('/mitra*', (req, res) => {
+router.all('/studi*', (req, res) => {
     let params
-    const headers = {headers:{
-            "Svc-Pass" : "leerling",
-            "Svc-User" : "schule",
-        }} 
-        const paths = req.path.replace("/mitra/", "/")
-    if(req.method.toLowerCase() == "get"){
+    const headers = { headers: req.headers }
+    const paths = req.path.replace("/studi/", "/")
+    if (req.method.toLowerCase() == "get") {
         params = [paths, headers]
-    }else {
-        params = [paths,req.body, headers ]
+    } else {
+        params = [paths, req.body, headers]
     }
-    
+
     api[req.method.toLowerCase()](...params).then(resp => {
         res.status(resp.status).send(resp.data)
     }).catch(e => {
         console.log(e)
         res.status(e.response.status).send(e.response.data)
     })
-    
 })
 
 module.exports = router
--- routers/router.js
+++ routers/router.js
@@ -3,9 +3,13 @@ var router = express.Router()
 var userService = require('./mbkm-user-service')
 var coreService = require('./mbkm-core-service')
 var mitraService = require('./mbkm-mitra-service')
+var stateService = require('./mbkm-state-service')
+var studiService = require('./mbkm-studi-service')
 
 router.use(userService)
 router.use(coreService)
 router.use(mitraService)
+router.use(stateService)
+router.use(studiService)
 
 module.exports = router
